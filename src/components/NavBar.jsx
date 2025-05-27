a25d843ca1a2ba7ce6d7834816c6cb8ca58e5544
Ricardo Hernandez <richhquim@gmail.com>
2025-05-27 13:28:02 -0600

    navBar2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -79,3 +79,26 @@ export default function NavBar({ onClose, isOpen }) {
 
     )
 }
+
+
+// import React from "react";
+// import NavBar from "./components/NavBar";
+// import { useState } from 'react'
+
+
+// export default function App() {
+//   const [isOpen, setIsopen] = useState(false)
+
+//   const toggleModal = () => {
+//     setIsopen((prev) => {
+//       return !prev
+//     })
+//   }
+//   return (
+//     <>
+//       <NavBar
+//         onClose={toggleModal}
+//         isOpen={isOpen} />
+
+//     </>)
+// }
