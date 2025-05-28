970b237ab1086f8dc52e4069e19b75610ea9df9e
TBejarano24 <bejaranotomas04@gmail.com>
2025-05-28 16:03:34 -0300

    se implementó la barra de categorías

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import { useState } from 'react'
+import { useState } from "react";
 import SimpleCarousel from "./components/Carousel";
+import CategoryBar from "./components/CategoryBar";
 
 export default function App() {
-  const [isOpen, setIsopen] = useState(false)
+  const [isOpen, setIsopen] = useState(false);
 
   const toggleModal = () => {
     setIsopen((prev) => {
-      return !prev
-    })
-  }
+      return !prev;
+    });
+  };
   return (
     <>
-      <NavBar
-        onClose={toggleModal}
-        isOpen={isOpen} />
+      <NavBar onClose={toggleModal} isOpen={isOpen} />
       <SimpleCarousel />
-    </>)
+      <CategoryBar />
+    </>
+  );
 }
-
