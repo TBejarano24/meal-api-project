84d4ad0a2c496314fac208f771eb2a2de4412599
TBejarano24 <bejaranotomas04@gmail.com>
2025-05-30 10:38:55 -0300

    Se añadió browser router a main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import NavBar from "./components/NavBar";
 import Home from "./pages/home/Home";
-import { BrowserRouter, Routes, Route, Link } from "react-router";
+import { Routes, Route, Link } from "react-router";
 import Receta from "./pages/receta/Receta";
 
 export default function App() {
   return (
     <>
-
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/receta" element={<Receta />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/receta" element={<Receta />} />
+      </Routes>
     </>
   );
 }
